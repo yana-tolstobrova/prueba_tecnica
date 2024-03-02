8668a4feddbad5d61eb4e21cde6ff8b7a8cb078f
Yana <126439762+yana-tolstobrova@users.noreply.github.com>
2024-03-02 21:49:32 +0100

    create new dog functionality

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -5,7 +5,7 @@
         <div class="row">
           <div class="col-md-12">
             <h2>Clasificador de Perros</h2>
-            <q-btn label="Añadir nuevo" class="text text-center" color="blue-10" />
+            <q-btn label="Añadir nuevo" class="text text-center" color="blue-10" @click="showModal = true" />
             <div v-for="dog in dogs" :key="dog.id">
               <img :src="dog.photo" alt="Dog Photo" style="width: 100px; height: 100px; object-fit: cover;">
               <div>
@@ -20,6 +20,23 @@
         </div>
       </div>
     </div>
+
+    <!-- Modal for adding a new dog -->
+    <q-dialog v-model="showModal">
+      <q-card>
+        <q-card-section>
+          <h4>Añadir Nuevo Perro</h4>
+          <input type="file" accept="image/*" @change="handleFileUpload" />
+          <q-input v-model="newDog.breed" label="Breed" />
+          <q-input v-model="newDog.size" label="Size" />
+          <q-input v-model="newDog.color" label="Color" />
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn label="Cancelar" color="negative" @click="showModal = false" />
+          <q-btn label="Crear" color="primary" @click="createDog" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -29,7 +46,14 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      dogs: []
+      dogs: [],
+      showModal: false,
+      newDog: {
+        photo: null,
+        breed: '',
+        size: '',
+        color: ''
+      }
     };
   },
   methods: {
@@ -40,6 +64,32 @@ export default {
         })
         .catch(error => {
           console.error('Error fetching dogs:', error);
+        });
+    },
+    handleFileUpload(event) {
+      this.newDog.photo = event.target.files[0];
+    },
+    createDog() {
+      let formData = new FormData();
+      formData.append('photo', this.newDog.photo);
+      formData.append('breed', this.newDog.breed);
+      formData.append('size', this.newDog.size);
+      formData.append('color', this.newDog.color);
+      
+      axios.post('http://localhost:8000/api/dogs', formData)
+        .then(response => {
+          const newDog = response.data.dog;
+          this.dogs.push(newDog);
+          this.showModal = false;
+          this.newDog = {
+            photo: null,
+            breed: '',
+            size: '',
+            color: ''
+          };
+        })
+        .catch(error => {
+          console.error('Error creating dog:', error);
         });
     }
   },

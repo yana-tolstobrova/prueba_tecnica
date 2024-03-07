f36e4e4adfc07b3740359fe88c0de76d3a9cf50f
Yana <126439762+yana-tolstobrova@users.noreply.github.com>
2024-03-07 16:23:50 +0100

    final script corrected

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 C='\033[0;36m'
-N='\033[0m' # No Color
+N='\033[0m'
 
 echo ${C}  "Launching docker containers."${N}
 cd backend
 docker-compose up -d
 echo ${C}  "Docker containers launched."${N}
-echo ${C} "Waiting for database container to initialize."${N}
-sleep 30
+echo ${C} "Installing composer dependencies."${N}
+docker exec backend composer install
 echo ${C}  "Building app inside containers."${N}
-docker exec -it backend php artisan migrate
-docker exec -it backend php artisan db:seed
+docker exec backend php artisan migrate
+docker exec backend php artisan db:seed
 cd ../frontend
 docker-compose up -d
 echo ${C}  "App is ready at localhost:8080." ${N}
